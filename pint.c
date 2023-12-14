de7384058b5cabfed862c62e86cfa5154cb6a12b
Ephyz8 <ziwoyaephraim@gmail.com>
2023-12-14 07:40:39 -0800

    trial

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * _pint - prints the top
+ * @hd: stack head
+ * @cntr: line_number
+ * Return: no return
+*/
+void _pint(stk_t **hd, unsigned int cntr)
+{
+	if (*hd == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", cntr);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*hd);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*hd)->n);
+}
